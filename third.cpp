e3ededfd99c78f115eb5c07a132b7f517e11f247
Me <127797343+SombraMail@users.noreply.github.com>
2023-04-20 02:51:48 +0300

    Add files via upload

---
+++ third.cpp
@@ -0,0 +1,144 @@
+﻿
+#include <iostream>
+
+template<typename T>
+class List
+{
+public:
+    List();
+    ~List();
+    void push_back(T data);
+    int GetSize() { return Size; }
+    void pop(int index);
+
+    T& operator[](const int index);
+
+private:
+    template<typename T>
+
+    class Node 
+    {
+    public:
+        Node* pNext;
+        T data;
+        Node(T data=T(), Node* pNext = nullptr)
+        {
+            this->data = data;
+            this->pNext = pNext;
+        }
+    };
+    int Size;
+    
+    Node<T> * head;
+
+};
+ 
+template<typename T>
+List<T>::List()
+{
+    Size = 0;
+    head = nullptr;
+}
+
+template<typename T>
+List<T>::~List()
+{
+
+}
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+    if (head == nullptr)
+    {
+        head = new Node<T>(data);
+    }
+    else 
+    {
+        Node<T> *current = this->head;
+        while (current->pNext != nullptr)
+        {
+            current = current->pNext;
+        }
+        current->pNext = new Node<T>(data);
+    }
+    Size++;
+}
+
+template<typename T>
+void List<T>::pop(int index)
+{
+    Node<T>* previous = this->head;
+    for (int i = 0; i < index - 1; i++)
+    {
+        previous = previous->pNext;
+    }
+
+    Node<T>* toDelete = previous->pNext;
+    previous->pNext = toDelete->pNext;
+    delete toDelete;
+
+    Size--;
+}
+
+
+
+template<typename T>
+T& List<T>::operator[](const int index)
+{
+    int counter = 0;
+    Node<T> *current = this->head;
+
+    while(current!= nullptr)
+    {
+        if (counter == index) 
+        {
+            return current->data;
+        }
+        current = current->pNext;
+        counter++;
+
+    }
+}
+
+
+
+int main()
+{   
+    using namespace std;
+   
+    List<string> prod;
+    string tobuy[11] = { "Apples","Porridge","Cucumbers", "Tomatoes","Orange juice","Peanuts","Peas","Salt","Sugar","Pepper","Shampoo"};
+    for (int i = 0; i < 11;i++) {
+        prod.push_back(tobuy[i]);
+    }
+
+
+    List<int> amount;
+    int money[11] = { 200,100,325,240,700,550,300,50,170,30,576};
+    for (int i = 0; i < 11; i++) {
+        amount.push_back(money[i]);
+    }
+
+    amount.pop(amount.GetSize() / 2);
+    prod.pop(prod.GetSize() / 2);
+ 
+
+   for (int i = 0; i < prod.GetSize(); i++)
+   {
+       cout << prod[i] << "->"<<amount[i]<< endl;
+       cout << endl;
+   }
+
+    
+    int sum=0;
+    for (int i = 0; i < prod.GetSize(); i++) 
+    {
+        sum += amount[i];
+    }
+    cout << "Overall:" <<sum;
+
+    return 0;
+
+
+}
---
+++ third222.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <string>
+
+template<typename T>
+class Node {
+public:
+    T data;
+    Node* prev, * next;
+    
+public:
+    Node(T data) {
+        this->data = data;
+        this->prev = this->next = NULL;
+    }
+    
+};
+template<typename T>
+class LinkedList {
+public:
+   
+    Node<T>* head, * tail;
+
+    LinkedList() {
+        
+        head = tail = NULL;
+    }
+    
+    Node<T>* push_back(T data) {
+        Node<T>* ptr = new Node<T>(data);
+
+        ptr->prev = tail;
+        if (tail != NULL)
+            tail->next = ptr;
+        if (head == NULL)
+            head = ptr;
+        tail = ptr;
+
+        return ptr;
+        
+    }
+    
+    Node<T>* getAt(int index) {
+        Node<T>* ptr = head;
+        int n = 0;
+
+        while (n != index) {
+            if (ptr == NULL)
+                return ptr;
+            ptr = ptr->next;
+            n++;
+        }
+
+        return ptr;
+    }
+
+    
+    
+    
+    void erase(int index) {
+        Node<T>* ptr = getAt(index);
+        if (ptr == NULL)
+            return;
+
+        Node<T>* left = ptr->prev;
+        Node<T>* right = ptr->next;
+        left->next = right;
+        right->prev = left;
+
+        delete ptr;
+    }
+    
+    
+};
+
+
+
+int main()
+{
+    using namespace std;
+    
+    int S = 0;
+    LinkedList<string> lst;
+    lst.push_back("Laptop");
+    lst.push_back("IPhone");
+    lst.push_back("Products");
+    lst.push_back("Netflix");
+    lst.push_back("Spotify");
+
+    
+    for (Node<string>* ptr = lst.head; ptr != NULL; ptr = ptr->next)
+    {
+        S++;
+    }
+    
+    lst.erase(S / 2);
+    
+    
+
+    LinkedList<int> L;
+    L.push_back(50000);
+    L.push_back(75000);
+    L.push_back(6000);
+    L.push_back(1000);
+    L.push_back(1500);
+
+    L.erase(S / 2);
+    for (Node<string>* ptr = lst.tail; ptr != NULL; ptr = ptr->prev) {
+
+        cout << ptr->data << '\t';
+        
+    }
+    cout << endl;
+    for (Node<int>* ptr = L.tail; ptr != NULL; ptr = ptr->prev) {
+       
+        cout << ptr->data << '\t';
+        
+    }
+    
+    
+}
